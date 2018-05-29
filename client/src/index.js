f231f5281f98e10d3b7f89fae131d327fffa125c
0toshigami <godonlyknows381@gmail.com>
2018-05-29 10:59:39 +0700

    update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HeaderCom from './components/HeaderComponent';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HeaderCom />, document.getElementsByClassName('super_container'));
 registerServiceWorker();
