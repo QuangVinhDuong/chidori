aa5127112c71531f3020b46d954ed9c29002cec0
0toshigami <godonlyknows381@gmail.com>
2018-05-29 10:30:10 +0700

    Add Header Com

---
+++ client/src/components/HeaderComponent.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+import TopBar from './HeaderItems/TopBar';
+import HeaderMain from './HeaderItems/HeaderMain';
+import MainNav from './HeaderItems/MainNav';
+import Menu from '.HeaderItems/Menu';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <header class="header">
+                <TopBar/>
+                <HeaderMain/>
+                <MainNav/>
+                <Menu/>
+            </header>
+        );
+    }        
+}
+
+export default HeaderComponent;
