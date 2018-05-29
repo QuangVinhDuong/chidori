85145c300b31dfe43abd1cfc4e1e9785dc075f9a
0toshigami <godonlyknows381@gmail.com>
2018-05-29 16:06:37 +0700

    Update Main Component

--- client/src/components/Main/MainComponent.jsx
+++ client/src/components/Main/MainComponent.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../Header/HeaderComponent';
 import Characteristics from '../Characteristics/CharacteristicsComponent';
+import PopularCategories from '../PopularCategories/PopularCategoriesComponent';
 
 class MainComponent extends Component {
     render() {
         return (
             <React.Fragment>
                 <Header />
-                <Characteristics />                        
+                <Characteristics />
+                <PopularCategories />                        
             </React.Fragment>            
         );
     }
