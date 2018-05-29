276e9a67f9cd933b74c4219267269cfbaf1047f8
0toshigami <godonlyknows381@gmail.com>
2018-05-29 18:30:51 +0700

    Added mongoose

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+
+// use mongoose with native Node Promise
+mongoose.Promise = global.Promise;
+
+// create connection to mongodb
+mongoose.connect('mongodb://localhost:27017/chidori')
+    .then(() => {
+        console.log("Connection successful");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/api/login", function(req, res) {
+
+
+app.post("/api/login", (req, res) => {
     var user = req.body.user;
     var pass = req.body.pass;
     if (user == "user1" && pass == "123") {
@@ -15,4 +30,4 @@ app.post("/api/login", function(req, res) {
     }
 });
 
-app.listen(3001, function(){console.log("Magic happened at port 3001");});
+app.listen(3001, () => {console.log("Magic happened at port 3001");});
