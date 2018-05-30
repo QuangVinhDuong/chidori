abae200ab910436c58d37db45495953188427044
0toshigami <godonlyknows381@gmail.com>
2018-05-31 01:00:06 +0700

    BUGS!!

--- client/src/components/Copyright/CopyrightComponent.jsx
+++ client/src/components/Copyright/CopyrightComponent.jsx
@@ -3,17 +3,17 @@ import React, {Component} from 'react';
 class CopyrightComponent extends Component {
     render() {
         return(
-            <div class="copyright">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
+            <div className="copyright">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
                                 
-                            <div class="copyright_container d-flex flex-sm-row flex-column align-items-center justify-content-start">
-                                <div class="copyright_content">
-                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            <div className="copyright_container d-flex flex-sm-row flex-column align-items-center justify-content-start">
+                                <div className="copyright_content">
+                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
                                 </div>
-                                <div class="logos ml-sm-auto">
-                                    <ul class="logos_list">
+                                <div className="logos ml-sm-auto">
+                                    <ul className="logos_list">
                                         <li><a href="#"><img src="images/logos_1.png" alt=""/></a></li>
                                         <li><a href="#"><img src="images/logos_2.png" alt=""/></a></li>
                                         <li><a href="#"><img src="images/logos_3.png" alt=""/></a></li>
--- client/src/components/Footer/FooterComponent.jsx
+++ client/src/components/Footer/FooterComponent.jsx
@@ -3,53 +3,53 @@ import React, {Component} from 'react';
 class FooterComponent extends Component {
     render() {
         return(
-            <footer class="footer">
-                <div class="container">
-                    <div class="row">
+            <footer className="footer">
+                <div className="container">
+                    <div className="row">
 
-                        <div class="col-lg-3 footer_col">
-                            <div class="footer_column footer_contact">
-                                <div class="logo_container">
-                                    <div class="logo"><a href="#">Chidori</a></div>
+                        <div className="col-lg-3 footer_col">
+                            <div className="footer_column footer_contact">
+                                <div className="logo_container">
+                                    <div className="logo"><a href="#">Chidori</a></div>
                                 </div>
-                                <div class="footer_title">Got Question? Call Us 24/7</div>
-                                <div class="footer_phone">+84 xxx xxx xxxx</div>
-                                <div class="footer_contact_text">
+                                <div className="footer_title">Got Question? Call Us 24/7</div>
+                                <div className="footer_phone">+84 xxx xxx xxxx</div>
+                                <div className="footer_contact_text">
                                     <p>227 Đường Nguyễn Văn Cừ</p>
                                     <p>Phường 4, Quận 5, Hồ Chí Minh</p>
                                 </div>
-                                <div class="footer_social">
+                                <div className="footer_social">
                                     <ul>
-                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-google"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                        <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i className="fab fa-youtube"></i></a></li>
+                                        <li><a href="#"><i className="fab fa-google"></i></a></li>
+                                        <li><a href="#"><i className="fab fa-vimeo-v"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="col-lg-2 offset-lg-2">
-                            <div class="footer_column">
-                                <div class="footer_title">Find it Fast</div>
-                                <ul class="footer_list">
+                        <div className="col-lg-2 offset-lg-2">
+                            <div className="footer_column">
+                                <div className="footer_title">Find it Fast</div>
+                                <ul className="footer_list">
                                     <li><a href="#">Computers & Laptops</a></li>
                                     <li><a href="#">Cameras & Photos</a></li>
                                     <li><a href="#">Hardware</a></li>
                                     <li><a href="#">Smartphones & Tablets</a></li>
                                     <li><a href="#">TV & Audio</a></li>
                                 </ul>
-                                <div class="footer_subtitle">Gadgets</div>
-                                <ul class="footer_list">
+                                <div className="footer_subtitle">Gadgets</div>
+                                <ul className="footer_list">
                                     <li><a href="#">Car Electronics</a></li>
                                 </ul>
                             </div>
                         </div>
 
-                        <div class="col-lg-2">
-                            <div class="footer_column">
-                                <ul class="footer_list footer_list_2">
+                        <div className="col-lg-2">
+                            <div className="footer_column">
+                                <ul className="footer_list footer_list_2">
                                     <li><a href="#">Video Games & Consoles</a></li>
                                     <li><a href="#">Accessories</a></li>
                                     <li><a href="#">Cameras & Photos</a></li>
@@ -59,10 +59,10 @@ class FooterComponent extends Component {
                             </div>
                         </div>
 
-                        <div class="col-lg-2">
-                            <div class="footer_column">
-                                <div class="footer_title">Customer Care</div>
-                                <ul class="footer_list">
+                        <div className="col-lg-2">
+                            <div className="footer_column">
+                                <div className="footer_title">Customer Care</div>
+                                <ul className="footer_list">
                                     <li><a href="#">My Account</a></li>
                                     <li><a href="#">Order Tracking</a></li>
                                     <li><a href="#">Wish List</a></li>
