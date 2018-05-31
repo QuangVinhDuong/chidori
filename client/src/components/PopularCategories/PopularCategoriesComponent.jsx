7c48e227fb5171d123f4b4ebf447354ec2d9d6a3
0toshigami <godonlyknows381@gmail.com>
2018-05-31 17:30:12 +0700

    update com

--- client/src/components/PopularCategories/PopularCategoriesComponent.jsx
+++ client/src/components/PopularCategories/PopularCategoriesComponent.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
+
 class PopularCategoriesComponent extends Component {
+
     render() {
         return (
             <div className="popular_categories">
