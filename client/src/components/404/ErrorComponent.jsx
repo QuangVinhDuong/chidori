72e6020bf5331a3b45bbc7e135aaafa8e3536561
Czer0C <1560602@student.hcmus.edu.vn>
2018-06-09 23:36:34 +0700

    update UI

--- client/src/components/404/ErrorComponent.jsx
+++ client/src/components/404/ErrorComponent.jsx
@@ -5,15 +5,15 @@ class ErrorComponent extends Component {
 	render() {
 		return (
 			<div className="bigBadBox">
-				<div class="header">
+				<div className="header">
 					<h1>Đã xảy ra lỗi!</h1>
 				</div>
-				<div class="w3-main">
-					<div class="agile-info">
+				<div className="w3-main">
+					<div className="agile-info">
 						<h2>404</h2>
 						<h3>Rất tiếc</h3>
 						<p>{this.props.message === null ? "Có vẻ bạn đã tới nhầm nơi!" : this.props.message}</p>
-						<Link to="/"><i class="fa fa-angle-double-left" aria-hidden="true"></i>Về trang chủ</Link>
+						<Link to="/"><i className="fa fa-angle-double-left" aria-hidden="true"></i>Về trang chủ</Link>
 					</div>
 				</div>
 			</div>
