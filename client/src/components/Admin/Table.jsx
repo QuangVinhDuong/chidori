7dac1eac4cdc744d3910293b80b4325c01fb560e
Czer0C <1560602@student.hcmus.edu.vn>
2018-06-13 20:48:54 +0700

    large scale update on admin UI

--- client/src/components/Admin/Table.jsx
+++ client/src/components/Admin/Table.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import './assets/css/Table.css';
 import ReactOverflowTooltip from 'react-overflow-tooltip';
+import Modal from 'react-responsive-modal';
 class Table extends Component {
   constructor(props) {
     super(props); 
     this.state = {
-      list: []
+      list: [],
+      open: false,
+      modalType: 1,
+      currentID: ''
     };
     this.truncateText = this.truncateText.bind(this);
+    
   }
-
+  
   componentDidMount() {
     this.getProduct();
+    
+    
   }
-
+  onOpenModal = () => { this.setState({ open: true }); };
+  onCloseModal = () => { this.setState({ open: false }); };
   getProduct() {
     fetch("/product/admin", {
       method: "GET",
@@ -24,17 +32,15 @@ class Table extends Component {
       .then((res) => res.json())
       .then((json) => {
         this.setState({ list: json.list });
-        console.log(this.state.list);
+        //console.log(this.state.list);
       });
-      this.handleCheck = this.handleCheck.bind(this);
-     
+    this.handleCheckDelivery = this.handleCheckDelivery.bind(this);
+    this.handleCheckDisable = this.handleCheckDisable.bind(this);
   }
-  handleCheck(event) {
+  handleCheckDelivery(event) {
     if (!window.confirm("R U SURE"))
       event.target.checked = !event.target.checked;
-      
     this.setState({checked: event.target.checked});
-    //console.log("After " + event.target.checked);
   }
   truncateText(string) {
     if (string.length > 45) {
@@ -45,20 +51,98 @@ class Table extends Component {
     }
   }
 
+  handleCheckDisable(event) {
+    if(!window.confirm("R U SURE"))
+      event.target.checked = !event.target.checked;
+  }
+
+  handleAdd = (e) => {
+    this.setState({ modalType: 1 });
+    this.onOpenModal();
+  }
+  handleUpdate = (e) => {
+    this.setState({modalType: 2, currentID: e.target.id});
+    this.onOpenModal();
+  }
+  handleSearch = () => {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("mainDataTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
 	render() {
 		const tableStyle = {
-			"vertical-align": "middle",
-			"text-align": "center"
+			"verticalAlign": "middle",
+			"textAlign": "center"
     }
-    const arr = this.state.list;
+    var arr = this.state.list;
+    const createForm = () => {
+      if (this.state.modalType === 1)
+        return (
+          <div>testing.....................</div>
+        )
+      else
+        return <div>
+            testing 2....................<div>
+              {this.state.currentID}
+            </div>
+          </div>;
+    }
 		return (
     <div className="col-md-12">
+      <Modal open={this.state.open} onClose={this.onCloseModal} center>
+        {createForm()}
+      </Modal>
+
+      {/* Modal confirm */}
+      <div class="modal fade" id="modalConfirmDelete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm modal-notify modal-danger" role="document">
+          <div class="modal-content text-center">
+            <div class="modal-header d-flex justify-content-center">
+              <p class="heading">Are you sure?</p>
+            </div>
+            <div class="modal-body">
+              <i class="fa fa-times fa-4x animated rotateIn" id="modal-x-icon"></i>
+            </div>
+            <div class="modal-footer flex-center">
+              <button className="btn btn-outline-danger">Yes</button>
+              <button className="btn btn-danger waves-effect" data-dismiss="modal">No</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Modal confirm */}
+
+
+      
+
+
         <div className="card table-with-switches" style={tableStyle}>
+          
           <div className="card-header ">
-            <h4 className="card-title">Danh sách sản phẩm</h4>
+            <h2 className="card-title"><b>Danh sách sản phẩm</b></h2>
+            <button className="btn btn-info col-sm-1" tooltip="Thêm" id="btnAdd" onClick={this.handleAdd} tooltip-position="buttom">
+              <i className="nc-icon nc-simple-add" />
+            </button>
+          <div className="form-group row">
+            <label htmlFor="searchInput" className="col-sm-1 col-form-label">Tìm kiếm:</label>
+            <div className="col-sm-8"><input type="text" className="form-control tab-search col-sm-3" id="searchInput" onKeyUp={this.handleSearch} placeholder="Nhập tên cần tìm.." title="Tìm kiếm theo tên sản phẩm" /></div>
+              </div>
+              
           </div>
           <div className="card-body table-full-width">
-            <table className="table table-striped">
+          <table className="table table-striped table-bordered" id="mainDataTable">
               <thead>
                 <tr>
                   <th className="text-center">#</th>
@@ -67,39 +151,47 @@ class Table extends Component {
                   <th className="text-center">Miêu tả</th>
                   <th className="text-center">Giao hàng</th>
                   <th className="text-center">Thao tác</th>
+                  <th className="text-center">Vô hiệu hóa</th>
                 </tr>
               </thead>
-              <tbody>
-                {
-                  arr.map((item, index) => (
-                    <tr>
-                      <td className="text-center">{index + 1}</td>
-                      <td className="text-center longtext" >
-                        <ReactOverflowTooltip title={item.productName}>
-                          <div>{this.truncateText(item.productName)}</div>
-                        </ReactOverflowTooltip>
-                      </td>
-                      <td className="text-center">{item.productType}</td>
-                      <td className="text-center longtext" >{item.description}</td>
-                      <td className="text-center">
-                        <label class="switch">
-                          <input type="checkbox" onChange={this.handleCheck} on />
-                          <span class="slider round"></span>
-                        </label>
-                      </td>
-                      <td className="td-actions text-center">
-                        <button className="btn btn-danger" tooltip="Xóa" tooltip-position="buttom"><i className="nc-icon nc-simple-remove"></i></button>
-                        <button className="btn btn-success" tooltip="Sửa" tooltip-position="buttom"><i className="nc-icon nc-settings-tool-66"></i></button>
-                      </td>
-                    </tr>
-                  ))
-                }
-               
+              <tbody id="mainDataTableBody">
+                {arr.map((item, index) => <tr>
+                    <td className="text-center">{index + 1}</td>
+                    <td className="text-center longtext">
+                      <ReactOverflowTooltip title={item.productName}>
+                        <div>{item.productName}</div>
+                      </ReactOverflowTooltip>
+                    </td>
+                    <td className="text-center">{item.productType}</td>
+                    <td className="text-center longtext">
+                      {item.description}
+                    </td>
+                    <td className="text-center ">
+                      <label className="switch">
+                        <input type="checkbox" onChange={this.handleCheckDelivery} id={`pship${item._id}`} disabled={item.isDeleted === false ? "" : "disabled"} on data-toggle="modal" data-target="#modalConfirmDelete"/>
+                        <span className="slider round" />
+                      </label>
+                    </td>
+                    <td className="td-actions text-center ">
+                      <button className="btn btn-danger" tooltip="Xóa" disabled={item.isDeleted === false ? "" : "disabled"} id={`pdel${item._id}`} tooltip-position="buttom" data-toggle="modal" data-target="#modalConfirmDelete">
+                        <i className="nc-icon nc-simple-remove" />
+                      </button>
+                      <button className="btn btn-success" tooltip="Sửa" disabled={item.isDeleted === false ? "" : "disabled"} id={`pfix${item._id}`} onClick={this.handleUpdate} tooltip-position="buttom">
+                        <i className="nc-icon nc-settings-tool-66" />
+                      </button>
+                    </td>
+                    <td className="text-center ">
+                      <label className="switch">
+                        <input type="checkbox" onChange={this.handleCheckDisable} id={`pdisa${item._id}`} on />
+                        <span className="slider round disable" />
+                      </label>
+                    </td>
+                  </tr>)}
               </tbody>
             </table>
           </div>
         </div>
-    </div>);
+      </div>);
 	}
 }
 
