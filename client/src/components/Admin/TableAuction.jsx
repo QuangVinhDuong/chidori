9b7efe0e10bfe27f711dad6c53f3dfa4557472de
Czer0C <1560602@student.hcmus.edu.vn>
2018-06-17 16:46:20 +0700

    new feature: add auction session

--- client/src/components/Admin/TableAuction.jsx
+++ client/src/components/Admin/TableAuction.jsx
@@ -8,36 +8,27 @@ class TableAuction extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			list: []
+			list: [],
+			open: false,
+			currentID: null,
+			modalType: 1, // 1 Thêm, 2 sửa
+			updateSessionID: null,
+			updateStartTime: null,
+			updateBidTime: null,
+			updateStatus: null,
+			updateInitPrice: null,
+			updateProductID: null,
+			productList: []
 		};
 	}
-
-	componentDidMount() { this.getAuction(); }
+	handleOnChangeSessionID = (e) => { this.setState({ updateSessionID: e.target.value});}
+	handleOnChangeProductID = (e) => { this.setState({ updateProductID: e.target.value});}
+	handleOnChangeStartTime = (e) => {this.setState({updateStartTime: e.target.value});}
+	handleOnChangeBidTime = (e) => {this.setState({updateBidTime: e.target.value});}
+	handleOnChangeInitPrice = (e) => {this.setState({updateInitPrice: e.target.value});}
+	componentDidMount() { this.getAuction(); this.getProduct(); }
 	onOpenModal = () => { this.setState({ open: true }); };
 	onCloseModal = () => { this.setState({ open: false }); };
-
-	getAuction() {
-		const obj = getFromStorage("login");
-
-		if (obj && obj.access_token) {
-			const { access_token } = obj;
-			fetch("/admin/auction", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${access_token}`
-				}
-			})
-			.then((res) => res.json())
-			.then((json) => {
-				this.setState({ list: json });
-				console.log(this.state.list);
-			});
-		}
-	}
-	handleAdd = () => {
-
-	}
 	handleSearch = () => {
 		var input, filter, table, tr, td, i;
 		input = document.getElementById("searchInput");
@@ -55,8 +46,161 @@ class TableAuction extends Component {
 			}
 		}
 	};
-	createForm = () => {
 
+	getAuction() {
+		const obj = getFromStorage("login");
+
+		if (obj && obj.access_token) {
+			const { access_token } = obj;
+			fetch("/admin/auction", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${access_token}`
+				}
+			})
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({ list: json.list });
+				console.log(this.state.list);
+			});
+		}
+	}
+	getProduct() {
+		const obj = getFromStorage("login");
+
+		if (obj && obj.access_token) {
+			const { access_token } = obj;
+			fetch("/admin/productListAU", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${access_token}`
+				}
+			})
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({ productList: json.plist });
+				//console.log(this.state.productList);
+			});
+		}
+	}
+	submitAuctionSession() {
+		const obj = getFromStorage('login');
+		if (obj && obj.access_token) {
+			var ID = this.state.currentID; // get current row ID
+			const { access_token } = obj;
+			fetch(`/admin/auction`, {
+				method: `${this.state.modalType === 1 ? "POST" : "PUT"}`,
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${access_token}`
+				},
+				body: JSON.stringify({
+					key: ID,
+					val: [
+						this.state.updateSessionID,
+						this.state.updateStartTime,
+						this.state.updateBidTime,
+						this.state.updateStatus,
+						this.state.updateInitPrice,
+						this.state.updateProductID
+					]
+				})
+			})
+				.then((res) => res.json())
+				.then((json) => {
+					if (json.success) {
+						//console.log(json);
+						if (this.state.modalType === 1) {
+							this.getAuction();
+						}
+						// else {
+						// 	var temp = JSON.parse(JSON.stringify(this.state.list));
+						// 	temp.forEach((i) => {
+						// 		if (i._id === ID) {
+						// 			(i.productName = this.state.updateName), (i.productType = this.state.updateType), (i.description = this.state.updateDesc), (i.productImage = this.state.updateLink);
+						// 		}
+						// 	});
+						// 	this.setState({ list: temp });
+						// }
+					} else {
+						alert("Xảy ra lỗi");
+					}
+				});
+		} 
+	}
+	handleAdd = () => {
+		this.setState({
+			open: true, 
+			modalType: 1, 
+			currentID: "",
+			updateSessionID: "12",
+			updateStartTime: "12:12",
+			updateBidTime: "12:0",
+			updateStatus: 0,
+			updateInitPrice: 1234,
+			updateProductID: this.state.productList[0]._id // Mặc định là cái đầu tiên.
+		})
+	}
+	handleSubmit = () => {
+		this.submitAuctionSession();
+		this.onCloseModal();
+	}
+	validate = () => {
+		return (this.state.updateSessionID === "" || this.state.updateStartTime === "" || this.state.updateBidTime === "" || this.state.updateStatus === "" | this.state.updateInitPrice === "" || this.state.updateProductID === "");
+	}
+	createForm = () => {
+		var t = this.state.productList;
+		return (
+          <div>
+            <b><center><h2>{this.state.modalType === 1 ? "Thêm phiên đấu giá" : "Chỉnh sửa thông tin phiên đấu giá"}</h2></center></b>
+            <form className="col-sm-12" onSubmit={(e) => {e.preventDefault();}}>
+              <div className="form-group row col-sm-12">
+                <label htmlFor="productName" className="col-sm-2 col-form-label">Mã phiên</label>
+                <div className="col-sm-8">
+                  <input type="number" className="form-control" required value={this.state.updateSessionID} onChange={this.handleOnChangeSessionID}/>
+                </div>
+                <div className={this.state.updateSessionID === "" ? "col-sm-2 warning" : "col-sm-2 ok"}>{this.state.updateSessionID === "" ? "Đừng bỏ trống" : "✔"}</div>
+              </div>
+
+              <div className="form-group row col-sm-12">
+                <label htmlFor="productName" className="col-sm-2 col-form-label">Tên sản phẩm</label>
+                <div className="col-sm-8">
+                  <select value={this.state.updateProductID} onChange={this.handleOnChangeProductID} className="form-control au">
+                    {
+						t.map((item, index) => (
+							<option value={item._id}>{item.productName}</option>
+						))
+					}
+                  </select>
+                </div>
+              </div>
+              <div className="form-group row col-sm-12">
+                <label htmlFor="productName" className="col-sm-2 col-form-label">Thời gian đấu</label>
+                <div className="col-sm-8">
+                  <input type="datetime-local" className="form-control" required value={this.state.updateStartTime} onChange={this.handleOnChangeStartTime}/>
+                </div>
+                <div className={this.state.updateStartTime === "" ? "col-sm-2 warning" : "col-sm-2 ok"}>{this.state.updateStartTime === "" ? "Đừng bỏ trống" : "✔"}</div>
+              </div>
+              <div className="form-group row col-sm-12">
+                <label htmlFor="productName" className="col-sm-2 col-form-label">Thời lượng</label>
+                <div className="col-sm-8">
+                  <input type="time" className="form-control" required value={this.state.updateBidTime} onChange={this.handleOnChangeBidTime}/>
+                </div>
+                <div className={this.state.updateBidTime === "" ? "col-sm-2 warning" : "col-sm-2 ok"}>{this.state.updateBidTime === "" ? "Đừng bỏ trống" : "✔"}</div>
+              </div>
+			  <div className="form-group row col-sm-12">
+                <label htmlFor="productName" className="col-sm-2 col-form-label">Giá khởi đầu</label>
+                <div className="col-sm-8">
+                  <input type="number" className="form-control" required value={this.state.updateInitPrice} onChange={this.handleOnChangeInitPrice}/>
+                </div>
+                <div className={this.state.updateInitPrice === "" ? "col-sm-2 warning" : "col-sm-2 ok"}>{this.state.updateInitPrice === "" ? "Đừng bỏ trống" : "✔"}</div>
+              </div>
+              <center><button className="btn btn-primary" disabled={this.validate() ? "disabled" : null} onClick={this.handleSubmit}>{this.state.modalType === 1 ? "Thêm" : "Cập nhật"}</button><button className="btn btn-danger" id="adminProductAdd" onClick={this.onCloseModal}>Thoát</button></center>
+            </form>
+          </div>
+        );
 	}
 	render() {
 		const tableStyle = {
@@ -115,17 +259,49 @@ class TableAuction extends Component {
 						<table className="table table-striped table-bordered" id="mainDataTable">
 							<thead>
 								<tr>
-									<th className="text-center">#</th>
-									<th>Tên sản phẩm</th>
-									<th>Loại</th>
-									<th className="text-center">Miêu tả</th>
-									<th className="text-center">Link ảnh</th>
+									<th className="text-center">Mã phiên</th>
+									<th className="text-center">Tên sản phẩm</th>
+									<th className="text-center">Thời gian bắt đầu</th>
+									<th className="text-center">Thời gian đấu</th>
+									<th className="text-center">Trạng thái</th>
+									<th className="text-center">Giá hiện tại</th>
+									<th className="text-center">Giá khởi đầu</th>
 									<th className="text-center">Thao tác</th>
+									<th className="text-center">Hủy phiên</th>
 									<th className="text-center">Vô hiệu hóa</th>
 								</tr>
 							</thead>
 							<tbody id="mainDataTableBody">
-								
+								{arr.map((item, index) => <tr>
+									<td className="text-center">{item.sessionID}</td>
+									<td className="text-center longtext">
+									<ReactOverflowTooltip title={item.p.productName}>
+										<div>{item.p.productName}</div>
+									</ReactOverflowTooltip>
+									</td>
+									<td className="text-center">{item.startTime}</td>
+									<td className="text-center">{item.bidTime}</td>
+									<td className="text-center">{item.aus.statusName}</td>
+									<td className="text-center">{item.initPrice}</td>
+									<td className="text-center">{item.currentPrice}</td>
+									<td className="td-actions text-center ">
+										<button className="btn btn-success" tooltip="Xem/Sửa" disabled={item.isDeleted} id={`afix${item._id}`} onClick={this.handleUpdate} tooltip-position="buttom">
+											<i className="nc-icon nc-settings-tool-66" />
+										</button>
+									</td>
+									<td className="text-center ">
+										<label className="switch">
+											<input type="checkbox" onClick={this.handleCheckDisable} disabled={!item.isDeleted ? item.status === 1 ? null : "disabled" : "disabled" } data-toggle ="modal" data-target="#modalConfirmDelete" id={`adis${item._id}`} on checked={item.isDisabled} />
+											<span className="slider round disable" />
+										</label>
+									</td>
+									<td className="text-center ">
+										<label className="switch">
+											<input type="checkbox" onClick={this.handleCheckDeleted} id={`adel${item._id}`} on checked={item.isDeleted}/>
+											<span className="slider round disable" data-toggle="modal" data-target="#modalConfirmDelete" />
+										</label>
+									</td>
+								</tr>)}
 							</tbody>
 						</table>
 					</div>
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,5 @@
 import express, {Router} from "express";
 import {urlencoded,	json} from "body-parser";
-
 import Product from '../models/Product';
 import AuctionSession from '../models/AuctionSession';
 import AuctionSessionStatus from '../models/AuctionSessionStatus';
@@ -8,7 +7,7 @@ import AuctionTicket from '../models/AuctionTicket';
 import AuctionTicketStatus from '../models/AuctionTicketStatus';
 import Account from '../models/Account';
 import Parameter from '../models/Parameter';
-
+import mongoose from 'mongoose';
 const router = Router();
 const app = express();
 const checkAuth = require('../middleware/check-auth');
@@ -89,16 +88,111 @@ router.post('/addProduct', checkAuth, (req, res, next) => {
 				success: data != null ? true : false
 			});
 		}
-	})
+	});
 });
 
+router.get('/productListAU', checkAuth, (req, res, next) => {
+	Product.aggregate([{
+		$project: {
+			_id: 1,
+			productName: 1
+		}
+	}], (err, out) => {
+		if (err) return next(err);
+		else {
+			return res.json({
+				plist: out
+			});
+		} 
+
+	})
+}) 
+
 router.get('/auction', checkAuth, (req, res, next) => {
-	// forEach(function (obj) {
-	// 	AuctionSession.insert(obj)
-	// });
+	
+	AuctionSession.aggregate([
+		{
+			$lookup: {
+				from: "product",
+				localField: "productID",
+				foreignField: '_id',
+				as: 'p'
+			}
+		},
+		{
+			$unwind: "$p"
+		},
+		{
+			$lookup: {
+				from: "auction_session_status",
+				localField: "status",
+				foreignField: "statusID",
+				as: "aus"
+			}
+		},
+		{
+			$unwind: "$aus"
+		},
+		{
+			$project: {
+				_id: 1,
+				sessionID: 1,
+				startTime: 1,
+				status: 1,
+				initPrice: 1,
+				currentPrice: 1,
+				bidTime: 1,
+				isDeleted: 1,
+				"p.productName": 1,
+				"aus.statusName": 1
+			}
+		}], (err, out) => {
+			
+			if (err) return next(err);
+			else {
+				return res.json({list: out});
+			}
+		}
+	)
+	
+	// au.save((err, data) => {
+	// 	if (err) return next(err);
+	// 	else {
+	// 		console.log(data);
+	// 		return res.json({
+	// 			success: data != null ? true : false
+	// 		});
+	// 	}
+	// })
 })
 router.post('/auction', checkAuth, (req, res, next) => {
-	console.log("post");
+	console.log(req.body);
+	const MongoClient = require('mongodb').MongoClient;
+	const url = "mongodb://localhost:27017/";
+	var ObjectId = mongoose.Types.ObjectId;
+	MongoClient.connect(url, function (err, db) {
+		if (err) throw err;
+		var dbo = db.db("chidori")
+
+		dbo.collection("auction_session").insert({
+			"sessionID": req.body.val[0],
+			"productID": new ObjectId(req.body.val[5]),
+			"startTime": req.body.val[1],
+			"bidTime": req.body.val[2],
+			"initPrice": req.body.val[4],
+			"currentPrice": req.body.val[4],
+			"status": req.body.val[3],
+			"isDeleted": false
+		}, (err, n) => {
+			if (err) console.log(err);
+			else {
+				//console.log(n);
+				return res.json({
+					success: n.result.ok == 1 ? true : false
+				});
+			}
+		})
+	});
 })
 router.put('/auction', checkAuth, (req, res, next) => {
 	console.log("put");
