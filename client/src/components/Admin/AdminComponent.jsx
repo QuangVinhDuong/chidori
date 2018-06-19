f5200fda42adef808f06b63aa78fb309e9cd3629
0toshigami <godonlyknows381@gmail.com>
2018-06-19 15:10:34 +0700

    :v

--- client/src/components/Admin/AdminComponent.jsx
+++ client/src/components/Admin/AdminComponent.jsx
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 import TableProduct from "./TableProduct";
 import Sidebar from './Sidebar';
+
 class AdminComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: []
-    };
-  }
-  render() {
-    return <React.Fragment>
-        <div className="wrapper">
-          <Sidebar />
-          <div className="main-panel">
-            <div className="content">
-              <div className="container-fluid">
-                <div className="row">
-                  <TableProduct />  
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: []
+        };
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <div className="wrapper">
+                    <Sidebar />
+                    <div className="main-panel">
+                        <div className="content">
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <TableProduct />  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </React.Fragment>;
-  }
+            </React.Fragment>
+        );
+    }
 }
 
 export default AdminComponent;
