aa8d0f943623441bbc104277645e8d1a7465b55e
0toshigami <godonlyknows381@gmail.com>
2018-06-19 14:34:03 +0700

    Update logic at product timer

--- client/src/components/Product/script.js
+++ client/src/components/Product/script.js
@@ -1,74 +1,8 @@
 import $ from 'jquery';
 
-var intervalID = null;
-
-function doCountDown() {
-    if($('.bestsellers_item').length) {
-        var timers = $('.bestsellers_item');
-        var timer = $(timers);
-        
-        var target = timer.find('.deals_timer_box');
-
-        var targetTime = target.data('target-time');
-        
-        // variables for time units
-        var hours, minutes, seconds;
-
-        var h = timer.find('.deals_timer_hr');
-        var m = timer.find('.deals_timer_min');
-        var s = timer.find('.deals_timer_sec');
-
-        var str = targetTime.split(':');
-                
-        //
-        hours = parseInt(str[0], 10);
-        minutes = parseInt(str[1], 10);
-        seconds = parseInt(str[2], 10);
-        
-        //
-        if (seconds > 0) {
-            seconds--;
-        }					
-
-        //
-        if (hours === 0 && minutes === 0 && seconds === 0) {
-            clearInterval(intervalID);
-            timer.remove();
-            $("#end").text("Đã kết thúc");
-        }
-
-        if (seconds === 0 && minutes > 0) {
-            seconds = 59;
-            minutes--;
-        }
-        if (minutes === 0 && hours > 0) {
-            minutes = 59;
-            hours--;
-        }
-
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
-        if (hours < 10) {
-            hours = '0' + hours;
-        }					
-        
-        // display results
-        h.text(hours);
-        m.text(minutes);
-        s.text(seconds); 
-        
-        targetTime = `${hours+':'+minutes+':'+seconds}`;
-        target.data('target-time', targetTime);
-    }
-}
-
-export function initTimer(flag) {
+export function initTimer(flag, sessionID = null) {
     if (flag === 1) {
-        intervalID = setInterval(doCountDown, 1000);    
+        intervalID = setInterval(doCountDown(sessionID), 1000);    
     }                                
     else if(flag === 0) {
         clearInterval(intervalID);
@@ -124,7 +58,7 @@ export function bidBoxWork() {
                             $("#current-price").text(data.currentPrice);
                             $("#bid-value").val(data.currentPrice);
                             $("#bid-value").data("min", data.currentPrice);
-                            initTimer(1);
+                            initTimer(1, sessionId);
                         },
                         complete: function(e) {                        
                             //const key = JSON.parse(localStorage.getItem("login"));                
@@ -168,4 +102,88 @@ export function bidBoxWork() {
 
         $("#bid-now-btn").one("click", clickHandler)         
     });    
+}
+
+var intervalID = null;
+
+function doCountDown(sessionID) {
+    if($('.bestsellers_item').length) {
+        var timers = $('.bestsellers_item');
+        var timer = $(timers);
+        
+        var target = timer.find('.deals_timer_box');
+
+        var targetTime = target.data('target-time');
+        
+        // variables for time units
+        var hours, minutes, seconds;
+
+        var h = timer.find('.deals_timer_hr');
+        var m = timer.find('.deals_timer_min');
+        var s = timer.find('.deals_timer_sec');
+
+        var str = targetTime.split(':');
+                
+        //
+        hours = parseInt(str[0], 10);
+        minutes = parseInt(str[1], 10);
+        seconds = parseInt(str[2], 10);
+        
+        //
+        if (seconds > 0) {
+            seconds--;
+        }					
+
+        // hết thời gian
+        if (hours === 0 && minutes === 0 && seconds === 0) {
+            clearInterval(intervalID);
+            timer.remove();
+            $("#end").text("Đã kết thúc");
+            $("#bid-now-btn").prop("disabled", true);
+            updateStatus(sessionID);
+        }
+
+        if (seconds === 0 && minutes > 0) {
+            seconds = 59;
+            minutes--;
+        }
+        if (minutes === 0 && hours > 0) {
+            minutes = 59;
+            hours--;
+        }
+
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        if (hours < 10) {
+            hours = '0' + hours;
+        }					
+        
+        // display results
+        h.text(hours);
+        m.text(minutes);
+        s.text(seconds); 
+        
+        targetTime = `${hours+':'+minutes+':'+seconds}`;
+        target.data('target-time', targetTime);
+    }
+}
+
+function updateStatus(sessionID) {
+	var obj = JSON.parse(localStorage.getItem("login"));
+
+	if (obj && obj.access_token && sessionID) {
+		const { access_token } = obj;		
+		$.ajax({
+			method: 'PUT',
+			beforeSend: function(req) {
+				req.setRequestHeader("Authorization", `Bearer ${access_token}`);
+			},
+			url: '/auction/updateStatusAuctionSession',
+			data: { ssID: sessionID }
+		});
+	}	
 }
