b0d6af8d86217a27d5f7685a24640b8ab0cf0686
Czer0C <1560602@student.hcmus.edu.vn>
2018-06-27 11:15:05 +0700

    Added Key for child array

--- client/src/components/Categories/Electronics.jsx
+++ client/src/components/Categories/Electronics.jsx
@@ -75,8 +75,8 @@ class Electronics extends Component {
                                                 <div className="bestsellers_item_container d-flex flex-row align-items-center justify-content-start">
                                                     <div className="bestsellers_image"><img src={item.productImage} alt="" /></div>
                                                     <div className="bestsellers_content">
-                                                        <div className="bestsellers_category"><a href="#">{item.productType}</a></div>
-                                                        <div className="bestsellers_name"><a href="#">Item {index}</a></div>
+                                                        <div className="bestsellers_category">{item.productType}</div>
+                                                        <div className="bestsellers_name">Item {index}</div>
                                                         <div className="bestsellers_price">{item.p[0].currentPrice} VND</div>
                                                         <div className="deals_timer_content ml-auto">
                                                             <div className="deals_timer_box clearfix" data-target-time={item.p[0].bidTime}>
